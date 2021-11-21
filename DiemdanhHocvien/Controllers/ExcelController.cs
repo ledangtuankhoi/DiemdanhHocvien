a8ec2e03c907e8bef1b6fa36507cb75237734b36
Tuấn Khôi <ledangtuankhoi@gmail.com>
2021-11-22 01:27:54 +0700

    export attendance for class

--- DiemdanhHocvien/Controllers/ExcelController.cs
+++ DiemdanhHocvien/Controllers/ExcelController.cs
@@ -4,6 +4,7 @@ using OfficeOpenXml.Style;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Web.Mvc;
@@ -42,26 +43,26 @@ namespace DiemdanhHocvien.Controllers
 
             //workSheet.Cells["A1:A2"].Value = "S.No";
             workSheet.Cells[1, 1, 2, 1].Merge = true;
-            workSheet.Cells[1, 1, 2, 1].Value = "S.No";
+            workSheet.Cells[1, 1, 2, 1].Value = "I";
 
             //workSheet.Cells["B1:B2"].Value = "S.No";
             workSheet.Cells[1, 2, 2, 2].Merge = true;
-            workSheet.Cells[1, 2, 2, 2].Value = "S.No";
+            workSheet.Cells[1, 2, 2, 2].Value = "Holy Name";
 
             //workSheet.Cells["C1:C2"].Merge = true;
             workSheet.Cells[1, 3, 2, 3].Merge = true;
-            workSheet.Cells[1, 3, 2, 3].Value = "S.No";
+            workSheet.Cells[1, 3, 2, 3].Value = "First Name";
 
             //workSheet.Cells["D1:D2"].Merge = true;
             workSheet.Cells[1, 4, 2, 4].Merge = true;
-            workSheet.Cells[1, 4, 2, 4].Value = "S.No";
+            workSheet.Cells[1, 4, 2, 4].Value = "LastName";
 
             //workSheet.Cells["D1:D2"].Merge = true;
             workSheet.Cells[1, 5, 2, 5].Merge = true;
-            workSheet.Cells[1, 5, 2, 5].Value = "S.No";
+            workSheet.Cells[1, 5, 2, 5].Value = "Id Student";
 
             //
-            int indexCol = 6;
+
             int firstDay = 0;
             List<int> dayOfWeek = classCurrently.dayOfWeek.Split(',').Select(Int32.Parse).ToList();
             // tim ngay dau tien da dang ky
@@ -75,39 +76,11 @@ namespace DiemdanhHocvien.Controllers
 
             }
 
-            // chạy từ ngày bắt đầu đến ngày kết thúc của lớp học 
-            for (DateTime date = classCurrently.startDate; date <= classCurrently.endDate; date = date.AddDays(1))
-            {
-                // gộp các dòng để đánh dấu 1 tuần
-                if ((int)date.Date.DayOfWeek == firstDay)
-                {
-                    workSheet.Cells[1, indexCol, 1, indexCol + dayOfWeek.Count()-1].Merge = true;
-                     workSheet.Cells[1, indexCol, 1, indexCol + dayOfWeek.Count()-1].Value = date.Date.Day + "  =>  " + date.Date.AddDays(6).ToString("dd-MM-yyyy");
-                }
 
-                //lọc ra ngày trong tuần đúng với thứ của tuần đã đăng ký
-                if (dayOfWeek.Contains(((int)date.DayOfWeek)) == true)
-                {
-
-                    //nếu ngày trong tuần bằng 0 thì là chủ nhật
-                    if ((int)date.DayOfWeek == 0)
-                    {
-                        workSheet.Cells[2, indexCol].Value = "CN";
-                        workSheet.Cells[3, indexCol].Value = date.Date.Day;
-
-                    }
-                    else
-                    {
-                        workSheet.Cells[2, indexCol].Value = (int)date.DayOfWeek + 1;
-                        workSheet.Cells[3, indexCol].Value = date.Date.Day;
-                    }
-                    indexCol++;
-                }
-
-            }
 
             //Body of table  
-            //  
+            //
+            int i = 6;
             int recordIndex = 4;
             foreach (var item in studentOfClass)
             {
@@ -119,9 +92,40 @@ namespace DiemdanhHocvien.Controllers
                 workSheet.Cells[recordIndex, 5].Value = item.id;
 
                 //ngày điểm danh 
-                
+                int indexCol = 6;
+
                 for (DateTime date = classCurrently.startDate; date <= classCurrently.endDate; date = date.AddDays(1))
                 {
+                    if (recordIndex == 4)
+                    {
+                        // gộp các dòng để đánh dấu 1 tuần
+                        if ((int)date.Date.DayOfWeek == firstDay)
+                        {
+                            workSheet.Cells[1, i, 1, i + dayOfWeek.Count() - 1].Merge = true;
+                            workSheet.Cells[1, i, 1, i + dayOfWeek.Count() - 1].Value = date.Date.Day + "  =>  " + date.Date.AddDays(6).ToString("dd-MM-yyyy");
+                        }
+
+                        //lọc ra ngày trong tuần đúng với thứ của tuần đã đăng ký
+                        if (dayOfWeek.Contains(((int)date.DayOfWeek)) == true)
+                        {
+
+                            //nếu ngày trong tuần bằng 0 thì là chủ nhật
+                            if ((int)date.DayOfWeek == 0)
+                            {
+                                workSheet.Cells[2, i].Value = "CN";
+                                workSheet.Cells[3, i].Value = date.Date.Day;
+
+                            }
+                            else
+                            {
+                                workSheet.Cells[2, i].Value = (int)date.DayOfWeek + 1;
+                                workSheet.Cells[3, i].Value = date.Date.Day;
+                            }
+                            i++;
+                        }
+                    }
+
+
                     //lọc ra ngày trong tuần đúng với thứ của tuần đã đăng ký
                     if (dayOfWeek.Contains(((int)date.DayOfWeek)) == true)
                     {
@@ -131,13 +135,15 @@ namespace DiemdanhHocvien.Controllers
 
                             if (b.order > 0)
                             {
-                                workSheet.Cells[recordIndex, indexCol].Value = "V- "+date.Day;
+                                workSheet.Cells[recordIndex, indexCol].Value = "";
                                 indexCol++;
 
                             }
                             else
                             {
-                                workSheet.Cells[recordIndex, indexCol].Value = "- " + date.Day;
+                                workSheet.Cells[recordIndex, indexCol].Value = "Lose";
+                                workSheet.Cells[recordIndex, indexCol].Style.Fill.PatternType = ExcelFillStyle.DarkHorizontal;
+                                workSheet.Cells[recordIndex, indexCol].Style.Fill.BackgroundColor.SetColor(Color.Orange);
                                 indexCol++;
                             }
                         }
@@ -145,9 +151,9 @@ namespace DiemdanhHocvien.Controllers
                         {
                             indexCol++;
                         }
- 
+
                     }
-                    
+
 
                 }
                 recordIndex++;
@@ -156,7 +162,7 @@ namespace DiemdanhHocvien.Controllers
             workSheet.Column(2).AutoFit();
             workSheet.Column(3).AutoFit();
             workSheet.Column(4).AutoFit();
-            string excelName = "studentsRecord";
+            string excelName = classCurrently.className + "-";
             using (var memoryStream = new MemoryStream())
             {
                 Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
