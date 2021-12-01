8f49d1fbccffe92ac4e31baacf8986f4cf9d2ef8
Tuấn Khôi <ledangtuankhoi@gmail.com>
2021-12-01 11:22:08 +0700

    finish

---
+++ DiemdanhHocVienAndroid2/app/src/main/java/com/example/diemdanhhocvienandroid2/api/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.diemdanhhocvienandroid2.api;
+
+import com.example.diemdanhhocvienandroid2.api.account.LoginRequest;
+import com.example.diemdanhhocvienandroid2.api.account.RegisterRequest;
+import com.example.diemdanhhocvienandroid2.api.account.RegisterResponse;
+import com.example.diemdanhhocvienandroid2.models.User;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UserService {
+
+    @POST("Login/")
+    Call<User> loginUser(@Body LoginRequest loginRequest);
+
+    @POST("register/")
+    Call<RegisterResponse> RegisterUser(@Body RegisterRequest registerRequest);
+}
